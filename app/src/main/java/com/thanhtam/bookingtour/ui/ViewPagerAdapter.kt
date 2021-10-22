cc1a06133e6692bc8dfff1f2e4a4ebbaf190b2f6
ThanhTam0509 <ngthanhtam5900@gmail.com>
2021-10-22 17:50:47 +0700

    done SplashScreen

---
+++ app/src/main/java/com/thanhtam/bookingtour/ui/ViewPagerAdapter.kt
@@ -0,0 +1,10 @@
+package com.thanhtam.bookingtour.ui
+
+/*
+///
+/// Project: Booking Tour
+/// Created by babyd on 10/22/2021.
+/// Copyright © 2018-2019 Beeknights Co., Ltd. All rights reserved.
+///
+*/class ViewPagerAdapter {
+}
